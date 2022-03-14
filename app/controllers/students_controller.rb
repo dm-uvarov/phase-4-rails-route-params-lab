6bde6962fe09409027ec077bc3372e8abc385e43
Jimmy <dm.a.uvarov@gmail.com>
2022-03-14 14:48:05 -0400

    commit tree 6ce23980a781f0a0af1a21a96c9f27437abf3880

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,29 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #    if (params[:first_name] || params[:last_name])
+  #     students = (Student.where(params[:first_name])&&Student.where(params[:last_name]))
+  #    else
+  #     students = Student.all
+  #    end
+  #   render json: students
+  # end
+
+  
+
   def index
-    students = Student.all
+    students = if params[:name]
+                 Student.looking_by_name(params[:name])
+               else
+                 Student.all
+               end    
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,17 @@
 class Student < ApplicationRecord
 
+  # scope :looking_by_name, ->(name) {
+  #   where("")
+  # }
+
+  def self.looking_by_name name
+    where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%","%#{name}%")
+  end
+
   def to_s
     "#{first_name} #{last_name}"
   end
+
+
 
 end
